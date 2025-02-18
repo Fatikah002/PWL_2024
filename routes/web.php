3ada648aadcff8dd056decb614c998df55eb972e
Fatikah Salsabilla <sfatikah2005@gmail.com>
2025-02-18 10:43:11 +0700

    P2_Praktikum 1_Langkah 14

--- routes/web.php
+++ routes/web.php
@@ -44,3 +44,8 @@ Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId){
 Route::get('/articles/{id}', function ($id){
     return 'Halaman dengan ID '.$id;
 });
+
+Route::get('/user/{name?}', function ($name=null){
+    return 'Nama saya ' .$name;
+}); 
+
