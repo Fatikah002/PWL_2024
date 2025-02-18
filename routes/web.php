dcf86ecab8c3fb2643192e19b472fc0054686efa
Fatikah Salsabilla <sfatikah2005@gmail.com>
2025-02-18 09:57:27 +0700

    P2_Praktikum 1_Langkah 7

--- routes/web.php
+++ routes/web.php
@@ -28,3 +28,7 @@ Route::get('/world', function () {
 Route::get('/', function () { 
     return 'Selamat Datang';
 });
+
+Route::get('/about', function () { 
+    return '2341720003 Fatikah Salsabilla';
+});
